e3a8d0214805ab00679c441c369011d7b0e461f7
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-06-04 15:02:39 +0100

    pass filepath as argument to CLI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_count_characters, get_sorted_char_counts
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,12 +7,17 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+    text = get_book_text(filepath)
     num_words = get_num_words(text)
     count_characters = get_count_characters(text)
     sorted_count_characters_list = get_sorted_char_counts(count_characters)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
